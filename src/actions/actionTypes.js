704d731875f73bdc9e29d1b03d2c3e48feefa115
Du Min Chen <dm.chen0113@gmail.com>
2021-09-09 20:50:56 -0400

    Finish redux two lab

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const NEXT_TODO_ID = "NEXT_TODO_ID";
+export const TODOS = "TODOS";
+export const TODOS_TOGGLE_COMPLETE = "TODOS_TOGGLE_COMPLETE";
---
+++ src/reducers/todosReducer.js
@@ -0,0 +1,21 @@
+import { TODOS, TODOS_TOGGLE_COMPLETE } from "../actions/actionTypes";
+
+const todosToggleComplete = (state, id) => {
+    const stateCopy = [...state];
+    const index = stateCopy.findIndex(todo => todo.id === id);
+    stateCopy[index].completed = !stateCopy[index].completed;
+    return stateCopy;
+}
+
+const todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case TODOS:
+            return [...state, action.todo];
+        case TODOS_TOGGLE_COMPLETE:
+            return todosToggleComplete(state, action.id);
+        default:
+            return state;
+    }
+}
+
+export default todosReducer;
