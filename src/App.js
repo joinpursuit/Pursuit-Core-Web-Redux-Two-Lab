ba4e90f0d7d0229120e3b4f13dec4a3c3d35f078
Alexander <alexanderl96@yahoo.com>
2021-10-11 18:31:58 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import "./App.css";
-import TodoApp from "./components/TodoApp";
+import TodoApp from "./components/todoApp";
 import rootReducer from "./reducers";
 
 function App() {
   return (
-    <Provider store={createStore(rootReducer)}>
+    <Provider store={createStore(rootReducer, devToolsEnhancer())}>
       <div className="app">
         <h1>Todos</h1>
         <TodoApp />
